ecb00dde70e0a0f64e17f65290de69c10596a3c3
Jack Medley <jack.medley@ed.ac.uk>
2016-01-06 13:37:20 +0000

    Inital commit

---
+++ Compile.sh
@@ -0,0 +1,12 @@
+# ~/bin/bash
+#"pdflatex" -interaction=nonstopmode ZJetsPaper.tex
+#"bibtex" ZJetsPaper
+#"pdflatex" -interaction=nonstopmode ZJetsPaper.tex
+#"pdflatex" -interaction=nonstopmode ZJetsPaper.tex
+#rm *.dvi *.ps *.log *.aux *.toc *.blg *.fdb_latexmk *.fls 2>/dev/null || true
+
+"pdflatex"  thesis.tex
+"bibtex" thesis
+"pdflatex"  thesis.tex
+"pdflatex"  thesis.tex
+rm *.dvi *.ps *.log *.aux *.toc *.blg *.fdb_latexmk *.fls 2>/dev/null || true
